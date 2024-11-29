ac50ed8bbdb643b83af73a613ac246fe1a1029a7
Rahmanyahya <rahmanyahya647@gmail.com>
2024-11-30 06:48:18 +0700

    add configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com','www.refugee-action.org.uk'], // Tambahkan hostname di sini
+}
 };
 
 export default nextConfig;
